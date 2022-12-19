dcbbaa163601ecdcf5dabc1fb92ad1651fb35add
Jonas4884 <hei.jonatana@gmail.com>
2022-12-19 09:25:11 +0300

    fix : play mapping endpoint

--- src/main/java/com/prog3_td2/prog3/controllers/response/PlayMapper.java
+++
@@ -1,2 +0,0 @@
-package com.prog3_td2.prog3.controllers.response;public class PlayMapper {
-}
---
+++ src/main/java/com/prog3_td2/prog3/controllers/response/PlayResponse.java
@@ -0,0 +1,19 @@
+package com.prog3_td2.prog3.controllers.response;
+
+import com.prog3_td2.prog3.model.TeamEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class PlayResponse {
+    private Long id;
+    private  String datetime;
+    private String stadium;
+    private String team1;
+    private String team2;
+}
