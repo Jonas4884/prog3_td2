1d32c79090d3202658212ddaee477b9b33681ede
Jonas4884 <hei.jonatana@gmail.com>
2023-01-11 11:19:53 +0300

    chore:final worked version

--- src/main/java/com/prog3_td2/prog3/controllers/response/PlayResponse.java
+++ src/main/java/com/prog3_td2/prog3/controllers/response/PlayResponse.java
@@ -1,12 +1,11 @@
 package com.prog3_td2.prog3.controllers.response;
 
-import com.prog3_td2.prog3.model.TeamEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Builder
 @AllArgsConstructor
