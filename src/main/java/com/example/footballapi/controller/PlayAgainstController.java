8008ab61b79f0c7141af85a9e807c251e72cedcd
Jonas4884 <hei.jonatana@gmail.com>
2023-01-16 11:14:58 +0100

    feat : add score to each team match

--- src/main/java/com/example/footballapi/controller/PlayAgainstController.java
+++ src/main/java/com/example/footballapi/controller/PlayAgainstController.java
@@ -5,20 +5,18 @@ import com.example.footballapi.controller.model.PlayResponse;
 import com.example.footballapi.service.MatchService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("matches")
-public class MatchController {
+public class PlayAgainstController {
     private final MatchService service;
     private final PlayMatchMapper mapper;
 
-    @GetMapping
-    public List<PlayResponse> getAll() {
+    @GetMapping("/matches")
+    public List<PlayResponse> getAllMatch() {
         return service.getAllMatch().stream().map(mapper::toRest).toList();
     }
 }
